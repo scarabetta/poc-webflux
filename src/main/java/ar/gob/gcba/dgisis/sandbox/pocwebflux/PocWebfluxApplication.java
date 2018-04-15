f2e0122249768586c8eb91fbfd1b79768ee96625
scarabetta <scarabetta@gmail.com>
2018-04-15 12:48:58 -0300

    Commit inicial

---
+++ src/main/java/ar/gob/gcba/dgisis/sandbox/pocwebflux/PocWebfluxApplication.java
@@ -0,0 +1,12 @@
+package ar.gob.gcba.dgisis.sandbox.pocwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocWebfluxApplication.class, args);
+	}
+}
---
+++ src/test/java/ar/gob/gcba/dgisis/sandbox/pocwebflux/PocWebfluxApplicationTests.java
@@ -0,0 +1,16 @@
+package ar.gob.gcba.dgisis.sandbox.pocwebflux;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PocWebfluxApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
